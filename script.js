275adbb199b548f48d7abf68c62006b4c15241ce
Matheus Zanutin <119885089+Mats057@users.noreply.github.com>
2024-06-05 14:17:08 -0300

    feat: carousel

--- script.js
+++ script.js
@@ -15,3 +15,36 @@ options.forEach((option) => {
     mobileHeaderIcon.src = "images/burger-menu.svg";
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.querySelector(".carousel-inner");
+    const prevButton = document.querySelector(".carousel-control.prev");
+    const nextButton = document.querySelector(".carousel-control.next");
+    const slides = document.querySelectorAll(".carousel-inner img");
+
+    let currentIndex = 0;
+    const totalSlides = slides.length;
+
+    function updateCarousel() {
+        const offset = -currentIndex * 100;
+        carousel.style.transform = `translateX(${offset}%)`;
+    }
+
+    prevButton.addEventListener("click", function () {
+        currentIndex = (currentIndex === 0) ? totalSlides - 1 : currentIndex - 1;
+        updateCarousel();
+    });
+
+    nextButton.addEventListener("click", function () {
+        currentIndex = (currentIndex === totalSlides - 1) ? 0 : currentIndex + 1;
+        updateCarousel();
+    });
+
+    updateCarousel();
+});
+
+let redirecionar = (link) => {
+    if(confirm("Deseja mesmo sair da página?")){
+        window.location.href = link;
+    }
+}
