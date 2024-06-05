69dfeb11601fdd896ec24b76a2ed9474fc3f2924
Matheus Zanutin <119885089+Mats057@users.noreply.github.com>
2024-06-05 17:36:00 -0300

    scrollAnimation

--- script.js
+++ script.js
@@ -1,50 +1,73 @@
-const mobileHeader = document.querySelector("#mobile-header");
-const mobileHeaderIcon = document.querySelector("#mobile-header-icon");
-const options = document.querySelectorAll(".option");
-
-mobileHeaderIcon.addEventListener("click", () => {
-  mobileHeader.classList.toggle("closed");
-  mobileHeaderIcon.src = mobileHeader.classList.contains("closed")
-    ? "images/burger-menu.svg"
-    : "images/close-md.svg";
-});
-
-options.forEach((option) => {
-  option.addEventListener("click", () => {
-    mobileHeader.classList.add("closed");
-    mobileHeaderIcon.src = "images/burger-menu.svg";
-  });
-});
-
 document.addEventListener("DOMContentLoaded", function () {
-    const carousel = document.querySelector(".carousel-inner");
-    const prevButton = document.querySelector(".carousel-control.prev");
-    const nextButton = document.querySelector(".carousel-control.next");
-    const slides = document.querySelectorAll(".carousel-inner img");
+  // Mobile menu toggle
+  const mobileHeader = document.querySelector("#mobile-header");
+  const mobileHeaderIcon = document.querySelector("#mobile-header-icon");
+  const options = document.querySelectorAll(".option");
 
-    let currentIndex = 0;
-    const totalSlides = slides.length;
+  mobileHeaderIcon.addEventListener("click", () => {
+    mobileHeader.classList.toggle("closed");
+    mobileHeaderIcon.src = mobileHeader.classList.contains("closed")
+      ? "images/burger-menu.svg"
+      : "images/close-md.svg";
+  });
 
-    function updateCarousel() {
-        const offset = -currentIndex * 100;
-        carousel.style.transform = `translateX(${offset}%)`;
-    }
-
-    prevButton.addEventListener("click", function () {
-        currentIndex = (currentIndex === 0) ? totalSlides - 1 : currentIndex - 1;
-        updateCarousel();
+  options.forEach((option) => {
+    option.addEventListener("click", () => {
+      mobileHeader.classList.add("closed");
+      mobileHeaderIcon.src = "images/burger-menu.svg";
     });
+  });
 
-    nextButton.addEventListener("click", function () {
-        currentIndex = (currentIndex === totalSlides - 1) ? 0 : currentIndex + 1;
-        updateCarousel();
+  // Animação das sections
+  const sections = document.querySelectorAll(".sections");
+
+  const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.3,
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target); // Remove observer after adding the class
+      }
     });
+  }, observerOptions);
 
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+
+  // Carousel
+  const carousel = document.querySelector(".carousel-inner");
+  const prevButton = document.querySelector(".carousel-control.prev");
+  const nextButton = document.querySelector(".carousel-control.next");
+  const slides = document.querySelectorAll(".carousel-inner img");
+  let currentIndex = 0;
+  const totalSlides = slides.length;
+
+  function updateCarousel() {
+    const offset = -currentIndex * 100;
+    carousel.style.transform = `translateX(${offset}%)`;
+  }
+
+  prevButton.addEventListener("click", function () {
+    currentIndex = currentIndex === 0 ? totalSlides - 1 : currentIndex - 1;
     updateCarousel();
+  });
+
+  nextButton.addEventListener("click", function () {
+    currentIndex = currentIndex === totalSlides - 1 ? 0 : currentIndex + 1;
+    updateCarousel();
+  });
+
+  updateCarousel();
 });
 
 let redirecionar = (link) => {
-    if(confirm("Deseja mesmo sair da página?")){
-        window.location.href = link;
-    }
-}
+  if (confirm("Deseja mesmo sair da página?")) {
+    window.location.href = link;
+  }
+};
